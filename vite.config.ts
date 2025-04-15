2bb67a25e2d7d37cde6f9ab16e795452b0fe9346
Jeroen Berkhof <jeroen@305.nl>
2025-04-15 15:44:19 +0200

    basic index page setup

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
+            input: ['resources/scss/app.scss', 'resources/js/app.tsx'],
             ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
         react(),
-        tailwindcss(),
     ],
     esbuild: {
         jsx: 'automatic',
